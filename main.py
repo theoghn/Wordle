736dcf27b9c2f06591d71900c63eae1f367a543f
theoghn <theotraian@yahoo.ro>
2022-11-16 18:27:19 +0200

    2

--- main.py
+++ main.py
@@ -1,39 +1,42 @@
-import copy
 import math
+def entropy(word):
+    for x in cuvinte:
+        for i in range(5):
+            if x[i] == word[i]:
+                v[i] = 0
+            elif x[i] in word:
+                v[i] = 1
+            else:
+                v[i] = 2
+        if str(v) in dict:
+            dict[str(v)] += 1
+
+    s = 0
+    t = 11454
+    for x in dict:
+        c = int(dict[x])
+        p = c/t
+        s += p*math.log2(1/p)
+    return s
+
+def SetUpdate(caz):
+    for x in cuvinte:
+        print("maine")
 file = open("cuvinte.txt","r")
-cuv = file.read()
-cuvinte = cuv.split()
+cuvinte = file.read().split()
 
 file = open("cazuri.txt","r")
-p = file.read()
-cazuri = p.split(";")
+cazuri = file.read().split(";")
 
 dict = {str(x): 0 for x in cazuri}
-print (dict)
-
-nr = 0
 v = [0, 0, 0, 0, 0]
-variante = [[0, 0, 0, 0, 0], [2, 2, 2, 2, 0]]
 tarei = "TAREI"
+caz = [1, 2, 1, 1, 1]
 
-for x in cuvinte:
-    print(x)
-    for i in range(5):
-        if x[i] == tarei[i]:
-            v[i] = 0
-        elif x[i] in tarei:
-            v[i] = 1
-        else:
-            v[i] = 2
-    if str(v) in dict:
-        dict[str(v)] += 1
-    else:
-        dict[str(v)] = 1
-s = 0
-t = 11454
-s2 = 0
-for x in dict:
-    c = int(dict[x])
-    p = c/t
-    s += p*math.log2(1/p)
-print(s)
+green = "\u1F7E9"
+yellow = "\u1F7E8"
+grey = "\u2B1B"
+
+print(entropy(tarei))
+set = {x for x in cuvinte}
+SetUpdate(2)
